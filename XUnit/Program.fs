cf0b83beb84825a282a7cb51a559c780bfde8a72
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2020-06-27 11:17:58 +0900

    Add chapter21

--- XUnit/Program.fs
+++ XUnit/Program.fs
@@ -7,9 +7,19 @@
 // [] 複数のテストを呼び出す
 // [] 収集したテスト結果を出力する
 // [x] WasRunで文字列をログに記録する
+// [ ] 失敗したテストを出力する
 
 open System
 open System.Reflection
+
+type TestResult() =
+    let mutable count = 0
+
+    member _.TestStarted () =
+        count <- count + 1
+
+    member _.Summary
+        with get () = sprintf "%d run, 0 failed" count
 
 [<AbstractClass>]
 type TestCase(name: string) =
@@ -20,10 +30,15 @@ type TestCase(name: string) =
     default _.TearDown () = ()
 
     member this.Run () =
+        let result = TestResult()
+        result.TestStarted ()
+
         this.Setup()
         let fn = this.GetType().GetMethod(name)
         fn.Invoke(this, [||]) |> ignore
         this.TearDown()
+
+        result
 
 type WasRun(name: string) =
     inherit TestCase(name)
@@ -37,18 +52,34 @@ type WasRun(name: string) =
     member _.TestMethod () =
         log <- "TestMethod"::log
 
+    member _.TestBokenMethod () =
+        raise (Exception())
+
     override _.TearDown() =
         log <- "TearDown"::log
+
 
 type TestCaseTest(name: string) =
     inherit TestCase(name)
 
     member _.TestTemplateMethod() =
         let test = WasRun("TestMethod")
-        test.Run()
-        assert (test.Log = ["Setup"; "TestMethod"; "TearDown"])
+        test.Run() |> ignore
+        assert (["Setup"; "TestMethod"; "TearDown"] = test.Log)
+
+    member _.TestResult () =
+        let test = WasRun("TestMethod")
+        let result = test.Run()
+        assert ("1 run, 0 failed" =  result.Summary)
+
+    member _.TestFailedResult()  =
+        let test = WasRun("TestBrokenMethod")
+        let result = test.Run()
+        assert ("1 run, 1 faild" = result.Summary)
 
 [<EntryPoint>]
 let main argv =
-    TestCaseTest("TestTemplateMethod").Run()
+    TestCaseTest("TestTemplateMethod").Run() |> ignore
+    TestCaseTest("TestResult").Run() |> ignore
+    // TestCaseTest("TestFailedResult").Run() |> ignore
     0
